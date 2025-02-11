3df5e8e6fa3f81ed9af3c45d097a79aa3cd9b363
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2025-02-12 02:02:00 +0530

    Update server.js

--- node-js-express-login-mongodb/server.js
+++ node-js-express-login-mongodb/server.js
@@ -13,7 +13,7 @@ const config = {
 };
 
 // const baseURL =  config[process.env.NODE_ENV || 'development'];
-const baseURL =  config['development'];
+const baseURL =  config['production'];
 
 // Middleware for handling CORS
 app.use((req, res, next) => {
